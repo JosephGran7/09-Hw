a73b588c3ddafaedd007c07194eccdc940b7e439
Joseph <josephgrant91739@gmail.com>
2021-01-19 18:21:29 -0800

    first major push. incorrect data is being pulled into the readMe

--- index.js
+++ index.js
@@ -1,19 +1,89 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+// const { message };
+// const prompt = () =>
+inquirer.prompt(
+  [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Project Title?',
+      // error back
+      validate: (value) => { if (value) { return true } else { return 'This field cannot be blank' } }
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Enter a description',
+      validate: (value) => { if (value) { return true } else { return 'This field cannot be blank' } }
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Installation Instructions',
+      validate: (value) => { if (value) { return true } else { return 'This field cannot be blank' } }
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Usage Information',
+      validate: (value) => { if (value) { return true } else { return 'This field cannot be blank' } }
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Contribution Guidelines',
+      validate: (value) => { if (value) { return true } else { return 'This field cannot be blank' } }
+    },
+    {
+      type: 'input',
+      name: 'test',
+      message: 'Test Instructions',
+      validate: (value) => { if (value) { return true } else { return 'This field cannot be blank' } }
+    },
 
-inquirer
-  .prompt([
-   "Project Title?"
+    // {
+    //   type: 'list',
+    //   name: 'category',
+    //   message: 'Business or Pleasure?',
+    //   choices: ['Work', 'Personal', 'Sandbox', 'Other'],
+    //   validate: (value) => { if (value) { return true } else { return 'This field cannot be blank' } }
+    // },
   ])
-  .then(answers => {
-    // Use user feedback for... whatever!!
-    fs.appendFile('log.txt', `${process.argv[2]}\n`, (err) =>
-    fs.writeFileSync('log.txt', answers);
-  })
-  .catch(error => {
-    if(error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else when wrong
-    }
+  .then(({
+    title,
+    description,
+    installation,
+    usage,
+    contributing,
+    test,
+
+  }) => {
+    // readME
+    const readMe = `${title}
+
+    // *[Description](#description)
+    // *[Installation](#installation)
+    // *[Usage](#usage)
+    // *[Contributing](#contributing)
+    // *[Test](#test)
+    
+    ${description}
+    ${installation}
+    ${usage}
+    ${contributing}
+    ${test}
+    `;
+
+    createNewFile(title, readMe);
+
+
   });
+function createNewFile(fileName, data) {
+  fs.writeFile(`./${fileName.toLowerCase().split('').join('')}.md`, data, (err) => {
+    if (err) {
+      console.log(err)
+    }
+    console.log('readMe Created!')
+  })
+}
